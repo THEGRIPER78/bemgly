4ecbd470706fa3c4e692d59cd4f71c7c5b03efbe
THEGRIPER78 <160645200+THEGRIPER78@users.noreply.github.com>
2024-02-20 09:44:11 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 basic.forever(function () {
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+    if (true) {
+    	
+    } else {
+    	
+    }
     basic.showLeds(`
-        . . # . .
-        . . # . .
-        . # . # .
         . . . . .
+        . # # # .
         . . . . .
+        . # # # .
+        # . # . #
         `)
 })
